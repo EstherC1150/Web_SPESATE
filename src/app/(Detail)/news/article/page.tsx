0e1646291db4446ccecedced54c42c124c130e5f
ksg2388 <ksg2388@naver.com>
2024-12-18 15:15:28 +0900

    Feat: 뉴스, 채용 페이지 라우터 추가가

---
+++ src/app/(Detail)/news/article/page.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  height: 100vh;
+  padding-top: 120px;
+  display: flex;
+  width: 100%;
+  justify-content: center;
+  background-color: #fff;
+`;
+
+const ContentContainer = styled.div`
+  display: flex;
+  max-width: 1200px;
+  background-color: #fff;
+  height: auto;
+  width: 100%;
+  margin: 40px;
+  margin-bottom: 80px;
+  margin-top: 100px;
+  flex-direction: column;
+`;
+
+const Title = styled.h4`
+  font-size: 44px;
+  font-weight: 600;
+  margin-bottom: 60px;
+`;
+
+const page = () => {
+  return (
+    <Container>
+      <ContentContainer>
+        <Title>뉴스룸</Title>
+      </ContentContainer>
+    </Container>
+  );
+};
+
+export default page;
