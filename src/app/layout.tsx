b17e1df11a82f5ce6aae08a049848ef07d30cfaa
daye choi <daye511@gmail.com>
2025-02-12 16:34:34 +0900

    Fix: 카카오 메타 이미지 크기 수정정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ export const metadata: Metadata = {
     type: "website",
     images: [
       {
-        url: "/images/logo-black.png",
+        url: "/images/logo-black-kakao.png",
         width: 800,
         height: 400,
       },
